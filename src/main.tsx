743ef14040fb60942a6e199ea33ef8f0bb0b323e
charukirti <charukirticc112002@gmail.com>
2026-03-02 22:12:54 +0530

    chore: install react-hot-toast library and setup

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,13 @@ import App from './App.tsx';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '@/router.tsx';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <Toaster position="top-right" />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
