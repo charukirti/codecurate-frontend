d26d0a0bd97fea31bf94c1a81b8d6e0f6fc5905b
charukirti <charukirticc112002@gmail.com>
2026-03-07 22:21:42 +0530

    feat(auth): redirect user to sign-in page after successful signout

--- src/features/auth/mutations/use-sign-out.ts
+++ src/features/auth/mutations/use-sign-out.ts
@@ -2,15 +2,18 @@ import { signOut } from '@/features/auth/api';
 import { authKeys } from '@/features/auth/mutations/queryKeys';
 import { clearAccessToken } from '@/lib/axios';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useNavigate } from '@tanstack/react-router';
 import toast from 'react-hot-toast';
 
 export function useSignOut() {
+  const navigate = useNavigate();
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: () => signOut(),
     onSuccess: () => {
       clearAccessToken();
       queryClient.invalidateQueries({ queryKey: authKeys.currentUser() });
+      navigate({ to: '/auth/sign-in' });
       toast.success('Signed out successfully');
     },
     onError: (error) => {
