ba67f800d42dec7f1890d237067c4b5118137fa4
charukirti <charukirticc112002@gmail.com>
2026-03-15 17:43:24 +0530

    feat: add loading skeleton for user reviews component

--- src/features/users/components/profile-page-skeleton.tsx
+++ src/features/users/components/profile-page-skeleton.tsx
@@ -11,10 +11,30 @@ function ProfileHero() {
   );
 }
 
+function UserReviews() {
+  return (
+    <div className="flex flex-col gap-6 animate-pulse">
+      <div className="h-6 w-48 bg-neutral-800 rounded" />
+      {[...Array(3)].map((_, i) => (
+        <div key={i} className="bg-neutral-900 border border-neutral-800 rounded-lg p-5 flex flex-col gap-3">
+          <div className="h-4 bg-neutral-800 rounded w-3/4" />
+          <div className="h-3 bg-neutral-800 rounded w-1/3" />
+          <div className="h-4 bg-neutral-800 rounded w-full" />
+          <div className="flex gap-2">
+            <div className="h-5 w-20 bg-neutral-800 rounded-full" />
+            <div className="h-5 w-20 bg-neutral-800 rounded-full" />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
 export function ProfilePageSkeleton() {
   return (
     <main className="container mx-auto px-4 py-8">
       <ProfileHero />
+      <UserReviews />
     </main>
   );
 }
