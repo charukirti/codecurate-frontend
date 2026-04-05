0f8b28ce9f70f6389f57022747a5d8d772c39560
charukirti <charukirticc112002@gmail.com>
2026-04-05 13:07:56 +0530

    feat(queryClient): create queryClient instance and update imports in main and router files

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { queryClient } from '@/router.tsx';
+import { queryClient } from '@/lib/queryClient.ts';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from 'react-hot-toast';
 
--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,12 @@
 import { createRouter } from '@tanstack/react-router';
-import { QueryClient } from '@tanstack/react-query';
 import { routeTree } from '@/routeTree.gen';
-
-export const queryClient = new QueryClient();
+import { queryClient } from '@/lib/queryClient';
 
 export const router = createRouter({
   routeTree,
   defaultPreload: 'intent',
   scrollRestoration: true,
-  context: { queryClient },
+  context: { queryClient: queryClient },
 });
 
 declare module '@tanstack/react-router' {
